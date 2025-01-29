4f162ab426361187f45e5a6ebdf0b59f5b1fed30
Thu Le <thule.hhs@gmail.com>
2025-01-29 13:07:48 -0500

    Updated lobby styling

--- client/src/components/modules/LobbyInput.jsx
+++ client/src/components/modules/LobbyInput.jsx
@@ -19,14 +19,18 @@ const LobbyInput = (props) => {
 
   return (
     <>
-      <input
-        type="text"
-        value={value}
-        placeholder="Enter lobby code"
-        onChange={handleChange}
-        onKeyDown={handleInput}
-      />
-      <button onClick={props.cancel}>Cancel</button>
+      <div>
+        <input
+          className="joinlobby-input"
+          type="text"
+          value={value}
+          placeholder="Enter lobby code"
+          onChange={handleChange}
+          onKeyDown={handleInput}
+        />
+        <button className="joinlobby-button" onClick={()=>{props.onSubmit(value)}}>Done</button>
+        <button className="joinlobby-button" onClick={props.cancel}>Cancel</button>
+      </div>
     </>
   );
 };

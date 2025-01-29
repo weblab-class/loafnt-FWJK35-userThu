17c5e2b9f3de7f89bd96cb11edd0281f91c7aa34
Thu Le <thule.hhs@gmail.com>
2025-01-29 10:44:33 -0500

    Styled gamefiles selection

--- client/src/components/modules/GameFile.jsx
+++ client/src/components/modules/GameFile.jsx
@@ -3,14 +3,18 @@ import React from "react";
 const GameFile = (props) => {
     return (
         <>
-        <div onClick={() => {
+        <div className="gamefile" onClick={() => {
             props.handleClick(props.slotKey);
         }}>
-            {
-                props.gameName === undefined || props.gameName === ""?
-                (<><h1>New Game</h1></>) : 
-                (<><h1>{props.gameName}</h1></>)
-            }
+            <img className="gamefile-img"/>
+            <div className="gamefile-content">
+                {
+                    props.gameName === undefined || props.gameName === ""?
+                    (<><h1>New Game</h1></>) : 
+                    (<><h1>{props.gameName}</h1></>)
+                }
+            </div>
+            
             
         </div>
         </>
